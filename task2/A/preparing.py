6dc36cc0e57396bdc98d059618e6d09d8f98d1b3
Dimon <konkov.da@phystech.edu>
2022-03-14 16:04:28 +0300

    add pragma once, delete useless files

--- task2/A/preparing.py
+++ task2/A/preparing.py
@@ -4,6 +4,7 @@ import sys
 output_file = sys.argv[1]
 fout = open(output_file, 'w')
 print("#include<iostream>", file=fout)
+print("#pragma once", file=fout)
 x = int(5 * random.random())
 print("const int amount = ", x, ";", sep='', file=fout)
 fout.close()
