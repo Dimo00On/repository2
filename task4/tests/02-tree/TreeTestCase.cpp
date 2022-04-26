871b22510b166f777fabd335ef93fa21c82e490f
Dimon <konkov.da@phystech.edu>
2022-04-26 15:02:59 +0300

    add more small tests

--- task4/tests/02-tree/TreeTestCase.cpp
+++ task4/tests/02-tree/TreeTestCase.cpp
@@ -5,30 +5,61 @@
 
 #include "TreeTestCase.h"
 #include "Tree.h"
+#include <fstream>
 
-TEST(Test02, test1) {
-    char name[] = "/tmp/dirXXXXXX";
-    char fileName[] = "/tmp/dirXXXXXX/fileXXXXXXX";
-    char otherName[] = "/tmp/dirXXXXXX/dirXXXXXX";
-    char* myPath = mkdtemp(name);
-    for (int i = 0; i < 14; ++i) {
-        otherName[i] = myPath[i];
-        fileName[i] = myPath[i];
+bool isCreatedFiles = false;
+std::filesystem::path uniqueFile;
+std::filesystem::path uniquePath;
+
+void createFiles() {
+    if (!isCreatedFiles) {
+        std::filesystem::path tempPath = std::filesystem::temp_directory_path();
+        std::string tempStr = tempPath.native();
+        size_t maxLength = 0;
+        for (const auto &entry: std::filesystem::directory_iterator(tempStr)) {
+            std::string file = entry.path().filename();
+            if (file.size() > maxLength) {
+                maxLength = file.size();
+            }
+        }
+        std::string uniqueStr = "/";
+        for (size_t i = 0; i < maxLength + 1; ++i) {
+            uniqueStr += "o";
+        }
+        std::filesystem::path uniqueName = uniqueStr;
+        std::cout << uniqueName << '\n';
+        uniquePath = tempStr + uniqueName.native();
+        std::filesystem::path uniqueDir = uniquePath.native() + "/dir";
+        uniqueFile = uniquePath.native() + "/file.txt";
+        std::filesystem::create_directories(uniqueDir);
+        std::ofstream file(uniqueFile);
+        isCreatedFiles = true;
     }
-    char* myOtherDir = mkdtemp(otherName);
-    int fd;
-    fd = mkstemp(fileName);
+}
+
+TEST(Test02, test1a) {
     try {
         auto error = GetTree("Hell world", true);
         ASSERT_EQ(false, true);
     }
     catch(...) {}
+}
+
+TEST(Test02, test1b) {
+    createFiles();
     try{
-        auto error = GetTree(fileName, false);
+        auto error = GetTree(uniqueFile, false);
         ASSERT_EQ(false, true);
     } catch(...) {}
-    FileNode notEmpty = GetTree(myPath, false);
-    FileNode empty = GetTree(myPath, true);
+}
+
+TEST(Test02, test1c) {
+    FileNode notEmpty = GetTree(uniquePath, false);
+    FileNode empty = GetTree(uniquePath, true);
     ASSERT_EQ(notEmpty == empty, false);
-    FilterEmptyNodes(notEmpty, myPath);
+}
+
+TEST(Test02, test1d) {
+    FileNode notEmpty = GetTree(uniquePath, false);
+    FilterEmptyNodes(notEmpty, uniquePath);
 }
